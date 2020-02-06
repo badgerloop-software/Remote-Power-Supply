c098fe9afc6939c1018315ddcb9520d550322f89
Eric Udlis <udlis.eric@gmail.com>
2020-02-05 20:54:20 -0600

    Inital Python Script and Command Prompt

---
+++ PowerSupplyDriver.py
@@ -0,0 +1,11 @@
+# # scpi controller
+# import easy_scpi as scpi
+
+# class PowerSupply( scpi.Instrument ):
+    
+#     def __init__(self, port):
+#         self = scpi.Instrument(port)
+#         self.connect()
+    
+#     def getVoltage(self):
+#         return self.source.voltage()
---
+++ main.py
@@ -0,0 +1,48 @@
+import easy_scpi as scpi
+import sys
+PORT = "COM4"
+
+z60 = scpi.Instrument(PORT)
+
+def getVoltage():
+  return z60.meas.volt.dc()
+
+def setVoltage(volts):
+  z60.source.voltage(volts)
+
+def connect():
+  z60.connect()
+
+## Commands Go Here
+print("Starting Command Shell... Connecting to Power Supply")
+z60.connect()
+if (z60.is_connected):
+  print("Power Supply Connected Running Command Shell")
+else:
+  print("Power Supply Can not Connect... Ending")
+  sys.exit()
+while(1):
+  text = input("Enter a command: ")
+  if (text == "connect"):
+    connect()
+    if (z60.is_connected):
+      print("Z60 is Connected!")
+    else:
+        print("Z60 failed to connect")
+  
+  if (text == "getVolts"):
+    print(getVoltage())
+  
+  if (text[:10] == "setVoltage"):
+    z60.init()
+    setVoltage(int(text[11:], base=10))
+  
+  if (text == "on"):
+    if(not z60.is_connected):
+      print("You must connect to the supply first!")
+    else:
+      z60.output.state('on')
+  
+  if (text == "off"):
+    z60.output.state ('off')
+  
