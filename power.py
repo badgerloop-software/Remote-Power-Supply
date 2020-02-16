6891ab09310b53cdc6586c9f402e6f643550356f
Ezra Boley <eboley@wisc.edu>
2020-02-16 16:45:43 -0600

    Added a more comprehensive test and some error checking

--- power.py
+++ power.py
@@ -6,15 +6,26 @@ import time
 
 class PowerSupply:
     _TERM = '\r'
-    
+    _err  = False
+
     def __init__(self):
         resMan = pyvisa.ResourceManager()
         resList = resMan.list_resources()
-        self._z60 = resMan.open_resource(resList[0])
+        
+        try:
+            self._z60 = resMan.open_resource(resList[0])
+        except IndexError:
+            print("Unable to find a power supply, shutting down")
+            self._err = True
+            return
+
         self._z60.read_termination = self._TERM
         self._z60.write_termination = self._TERM
         self._z60.write("INST:NSEL 4")
         self._z60.write("SYST:REM REM")
+
+    def initFail(self):
+        return self._err
 
     def turnOn(self):
         self._z60.write("OUTP ON")
@@ -31,4 +42,5 @@ class PowerSupply:
 
     def getCurrent(self):
         return self._z60.query("MEAS:CURR:DC?")
-    
+   
+   
---
+++ z60.py
@@ -0,0 +1,37 @@
+import power
+import argparse
+import sys
+
+VALID_VOLTAGES = [0, 24]    ## Add if we need a different voltage
+HELP_MSG = "Customizable power supply control for automated testing, run with no args to turn off"
+
+def main(onoff, voltage, info):
+    
+    z60 = power.PowerSupply()
+    if z60.initFail():
+        return
+
+    if onoff == 'on':
+        pass
+        z60.setVoltage(voltage)
+        z60.turnOn()
+    elif onoff == 'off':
+        pass
+        z60.turnOff()
+
+    if info:
+        v = z60.getVoltage()
+        c = z60.getCurrent()
+        print("CURRENT: " + c)
+        print("VOLTAGE: " + v)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=HELP_MSG)
+    parser.add_argument('-v', '--volt', nargs=1, default=0, type=int, choices=VALID_VOLTAGES)
+    parser.add_argument('-p', '--power', nargs=1, default='off', choices=['off', 'on'])
+    parser.add_argument('-i', '--info', action='store_true')
+    args = parser.parse_args()
+
+    main(args.power, args.volt, args.info)
+
